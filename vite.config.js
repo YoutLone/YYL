e1935261a440c6d21e0ee91c45fc8b69d1a77ce2
Next-Innovations-TMH <thanmyohtet@TMH-2.local>
2025-02-09 15:18:52 +0630

    Update vite config file

--- vite.config.js
+++ vite.config.js
@@ -4,13 +4,4 @@ import react from '@vitejs/plugin-react'
 // https://vitejs.dev/config/
 export default defineConfig({
   plugins: [react()],
-  build: {
-    outDir: 'dist', // Ensure the build output goes to the 'dist' folder
-    // You can add other optimizations like minification or CSS splitting here
-  },
-  server: {
-    // Vite's dev server setup (usually for local dev, can leave as default for most cases)
-    port: 3000,
-    open: true,  // Automatically open the app in the browser when running locally
-  }
 })
